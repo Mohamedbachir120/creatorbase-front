b4551b29f64f5e20019b903f9e324bea0aa1298d
Mohamedbachir120 <mohamedbhadjadji@gmail.com>
2025-08-28 20:13:58 +0100

    remove sidebar

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -116,16 +116,7 @@ const Sidebar: FC<SidebarProps> = ({ activePage, onNavigate, isOpen, onToggle })
       iconPath:
         'M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z',
     },
-    {
-      name: 'Mes campagnes',
-      iconPath:
-        'M9 2a1 1 0 000 2h2a1 1 0 100-2H9z M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z',
-    },
-    {
-      name: 'Analyses',
-      iconPath:
-        'M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z',
-    },
+ 
     {
       name: 'Facturation',
       iconPath:
@@ -799,10 +790,7 @@ const DashboardPage: FC = () => {
             <DatabaseContent />
           </ProtectedContent>
         );
-      case 'Mes campagnes':
-        return <ComingSoonContent />;
-      case 'Analyses':
-        return <ComingSoonContent />;
+     
       case 'Facturation':
         return <PricingContent />;
       default:
